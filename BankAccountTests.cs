46d6a95abbbe3a585de6f24237683f407ecb8e3d
AndyBrizhak <andrewbrizhak@gmail.com>
2019-08-16 18:46:49 +0800

    + AAA

--- BankAccountTests.cs
+++ BankAccountTests.cs
@@ -7,8 +7,10 @@ namespace UnitTests
         [Test]
         public void BankAccountShouldIncreaseOnPositiveDeposite()
         {
-            
-        }
+            var ba = new BankAccount(100);
+            ba.Deposit(100);
+            Assert.That(ba.Balance, Is.EqualTo(200));
+        } 
         
     }
 }
---
+++ Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void MyMethod()
+        {
+            Assert.Fail();
+        }
+    }
+}
