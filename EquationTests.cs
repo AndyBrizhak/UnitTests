d9a09728e56f40d884b5dd6c1a0a35de47cf6389
AndyBrizhak <andrewbrizhak@gmail.com>
2019-08-17 11:01:38 +0800

    + dotCover

--- EquationTests.cs
+++ EquationTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace UnitTests
@@ -10,5 +11,14 @@ namespace UnitTests
         {
             var result = Solve.Quadratic(1, 10, 16);
         } 
+        
+        [Test]
+                public void Test2()
+                {
+                    Assert.Throws<Exception>(() =>
+                    {
+                        Solve.Quadratic(1, 1, 1);
+                    });
+                } 
     }
 }
