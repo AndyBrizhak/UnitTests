39dc10e8fa01242e1764aba5cece8bc98188fc6b
AndyBrizhak <andrewbrizhak@gmail.com>
2019-08-18 08:58:58 +0800

    dotMemory

--- EquationTests.cs
+++ EquationTests.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using JetBrains.dotMemoryUnit;
 using NUnit.Framework;
 
 namespace UnitTests
@@ -20,5 +22,23 @@ namespace UnitTests
                         Solve.Quadratic(1, 1, 1);
                     });
                 } 
+                
+          [Test]
+                  public void Test3()
+                  {
+//                      var result = Solve.Quadratic(1, 10, 16);
+                      var checkpoint1 = dotMemory.Check();
+                        //
+
+                        var checkpoint2 = dotMemory.Check(memory =>
+                        {
+                            Assert.That(memory.GetTrafficFrom(checkpoint1)
+                                .Where(obj => obj.Interface.Is<IEnumerable<int>>())
+                                .AllocatedMemory.SizeInBytes, Is.LessThan(1000));
+                        });
+
+
+
+                  }       
     }
 }
